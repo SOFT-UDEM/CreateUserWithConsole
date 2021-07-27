d44008fd02eed82424e7da0d1d6c7d9e6e37652c
carlosqdev <carlosquintero95nic@gmail.com>
2021-07-27 17:07:26 -0600

    Added programing for create user

--- CreateUserWithConsole/Program.cs
+++ CreateUserWithConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CreateUserWithConsole.Models;
 
 namespace CreateUserWithConsole
 {
@@ -10,6 +11,33 @@ namespace CreateUserWithConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("¡Welcome to create user!");
+            Console.WriteLine("Please wait");
+            using (Models.bdsoftEntities database = new Models.bdsoftEntities())
+            {
+                try
+                {
+                    Models.Usuario oUser = new Models.Usuario()
+                    {
+                        Nombre = "Usuario",
+                        Apellido = "de Prueba",
+                        Cargo = "Administrador",
+                        RolDeAcceso = "Admin",
+                        UserName = "prueba",
+                        Password = Encriptation.GetSHA256("prueba"),
+                        Observacion = "Usuario de prueba generado por consola"
+                    };
+                    database.Usuarios.Add(oUser);
+                    database.SaveChanges();
+                    Console.WriteLine("User created successfully");
+                    Console.WriteLine("Keypress enter for exit");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error" + ex.Message);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
